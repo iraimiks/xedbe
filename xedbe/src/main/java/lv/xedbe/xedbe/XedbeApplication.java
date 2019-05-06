ab59a237622a6ba983e298d31b251198139187a9
iraimiks <r.bortascenoks@gmail.com>
2019-05-06 18:39:27 +0300

    first commit

---
+++ xedbe/src/main/java/lv/xedbe/xedbe/XedbeApplication.java
@@ -0,0 +1,13 @@
+package lv.xedbe.xedbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XedbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XedbeApplication.class, args);
+	}
+
+}
---
+++ xedbe/src/test/java/lv/xedbe/xedbe/XedbeApplicationTests.java
@@ -0,0 +1,16 @@
+package lv.xedbe.xedbe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XedbeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
